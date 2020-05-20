fdfa3e3ff87fd6b5017c972ea0c1c551eaacb16c
LENOVO <LENOVO@LAPTOP-K97GS86T>
2020-05-20 12:40:39 +0530

    First Commit

---
+++ src/com/default1/AdditionExa.java
@@ -0,0 +1,14 @@
+package com.default1;
+
+public class AdditionExa {
+
+	public static void Add() {
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) {
+
+		AdditionExa.Add();
+	}
+
+}
